f26de637712a4d22543a1c2f7d592b79adf50c10
falsehex <falsehex@gmail.com>
2020-11-14 16:23:09 +1100

    Add files via upload

--- download/laconic.pl
+++ download/laconic.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#  laconic.pl - Version 1.0.0  07 Nov 20
+#  laconic.pl - Version 1.0.1  14 Nov 20
 #  Laconic - Simple HTTP Server
 #  Copyright 2020 Del Castle
 #
@@ -8,19 +8,23 @@
 #    cp laconic.pl /usr/local/bin/
 #    useradd -r -s /usr/sbin/nologin www
 #    touch /var/log/http.log
-#    chown www:www /usr/local/bin/laconic.pl /var/log/http.log
-#    chmod 700 /usr/local/bin/laconic.pl
+#    mkdir /var/www
+#    chown www:www /usr/local/bin/laconic.pl /var/log/http.log /var/www
+#    chmod 750 /usr/local/bin/laconic.pl
 #    chmod 644 /var/log/http.log
+#    chmod 750 /var/www
 #    Add to /etc/crontab: * *  * * *  www  ps -ef | grep laconic.pl | grep -v grep >/dev/null || laconic.pl >/dev/null 2>&1 &
 #    Port forward 80 to 8123 on firewall
 
+use autodie;
 use strict;
 use warnings;
 use threads;
 use Fcntl;
+use File::Find;
 use IO::Socket::INET;
 
-my $strKnock = 'itsme';  #code word to open firewall
+my $strKnock = 'secret';  #code word to open firewall
 my @txtDays = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
 my @txtMonths = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 my %valMonths = qw(january 1 february 2 march 3 april 4 may 5 june 6 july 7 august 8 september 9 october 10 november 11 december 12);
@@ -35,6 +39,71 @@ my $sockListen = IO::Socket::INET->new(LocalPort => 8123,
 $SIG{INT} = sub { close($sockListen); };
 $SIG{TERM} = sub { close($sockListen); };
 
+my $icoFavicon = pack('H*', '000001000100101000000100200068040000160000002800000010000000200000000100200000000000000000000000000000000000000000000000000000000000000080ff000080ff0000ffff000080ff008080ff00ffffff00ffffff00ffffff00ffffff008080ff008000ff00ff00ff008000ff008000ff0000000000000000000080ff000080ff0000ffff000080ff008080ff00ffffff00ffffff00ffffff00ffffff008080ff008000ff00ff00ff008000ff008000ff00000000000000400000bfff000080ff0000ffff004080ff008080ff00ffffff00ffffff00ffffff00ffffff008080ff008040ff00ff00ff008000ff00bf00ff00000040000000800000ffff000080ff0000ffff008080ff008080ff00ffffff00ffffff00ffffff00ffffff008080ff008080ff00ff00ff008000ff00ff00ff00000080000055bf0000ffff000080ff0000bfff00bfbfff008080ff00ffffff00ffffff00ffffff00ffffff008080ff00bfbfff00bf00ff008000ff00ff00ff005500bf000080ff0000ffff000080ff000080ff00ffffff008080ff00ffffff00ffffff00ffffff00ffffff008080ff00ffffff008000ff008000ff00ff00ff008000ff000080ff0000ffff0000ffff000080ff00ffffff00ffffff00ffffff00ffffff00ffffff00ffffff00ffffff00ffffff008000ff00ff00ff00ff00ff008000ff000000800000ffff0000ffff0000ffff008080ff00ffffff00ffffff00ffffff00ffffff00ffffff00ffffff008080ff00ff00ff00ff00ff00ff00ff000000800000000000000080000055bf0000ffff0000bfff000080ff00bfbfff00ffffff00ffffff00bfbfff008000ff00bf00ff00ff00ff005500bf0000008000000000000000000000000000000000000080ff0000ffff000080ff0000008000ffffff00ffffff00000080008000ff00ff00ff008000ff0000000000000000000000000000000000000000000055bf0000ffff0000ffff0000bfff00bfbfff00ffffff00ffffff00bfbfff00bf00ff00ff00ff00ff00ff005500bf000000000000000000000000000000400000bfff0000ffff0000ffff004080ff00ffffff00ffffff00ffffff00ffffff008040ff00ff00ff00ff00ff00bf00ff000000400000000000000000000000800000ffff0000ffff0000ffff008080ff00ffffff00ffffff00ffffff00ffffff008080ff00ff00ff00ff00ff00ff00ff000000800000000000000000000000400000bfff0000ffff0000ffff004080ff00ffffff00ffffff00ffffff00ffffff008040ff00ff00ff00ff00ff00bf00ff00000040000000000000000000000000000055bf0000ffff0000ffff0000bfff00bfbfff00ffffff00ffffff00bfbfff00bf00ff00ff00ff00ff00ff005500bf0000000000000000000000000000000000000000000055bf000080ff000055bf00000040008080ff008080ff00000040005500bf008000ff005500bf0000000000000000000000008001000080010000800100008001000080010000000000000000000080010000e0070000e2470000e0070000c0030000c0030000c0030000e0070000f66f0000');
+my %filesRaw;
+my %filesMod;
+
+#create text file
+sub createFile
+{
+  if (! -e $_[0])
+  {
+    my $outFile;
+    open($outFile, '>', $_[0]);  #open new file
+    print $outFile $_[1];  #write contents to file
+    close($outFile);
+  }
+}
+
+createFile('/var/www/favicon.ico', $icoFavicon);
+createFile('/var/www/index.html', '<html><head><title>Under Construction</title></head><body><p>Website Currently Unavailable - Under Construction</p></body></html>');
+createFile('/var/www/404.html', '<html><head><title>Page Not Found</title></head><body><h2>404 - Page Not Found</h2></body></html>');
+createFile('/var/www/pi.html', '<html><head><title>Laconic</title></head><body><h3>&pi;</h3></body></html>');
+
+#file find
+sub wanted
+{
+  if (-e -f $File::Find::name)
+  {
+    my $fileName = $File::Find::name;
+    $fileName =~ s/\/var\/www//;  #remove path
+    my $inFile;
+    open($inFile, '<:raw', $File::Find::name);
+    read($inFile, $filesRaw{$fileName}, -s $inFile);  #read full file
+    close($inFile);
+    $filesMod{$fileName} = (stat($File::Find::name))[9];  #file modified date
+  }
+}
+
+finddepth(\&wanted, '/var/www');
+
+#log date
+sub timeLog
+{
+  my ($valSec, $valMin, $valHour, $valMday, $valMon, $valYear, $valWday, $valYday, $valIsdst) = gmtime();
+  return sprintf('%s %02d %02d:%02d:%02d', $txtMonths[$valMon], $valMday, $valHour, $valMin, $valSec);
+}
+
+#server date
+sub timeHTTP
+{
+  my ($valSec, $valMin, $valHour, $valMday, $valMon, $valYear, $valWday, $valYday, $valIsdst) = gmtime($_[0]);
+  return sprintf('%s, %02d %s 20%02d %02d:%02d:%02d GMT', $txtDays[$valWday], $valMday, $txtMonths[$valMon], $valYear - 100, $valHour, $valMin, $valSec);
+}
+
+#determine MIME type
+sub mimeHTTP
+{
+  my ($fileExt) = $_[0] =~ /(\.[^.]+)$/;  #file extension
+  if ($fileExt eq '.html') { return 'text/html'; }
+  elsif ($fileExt eq '.css') { return 'text/css'; }
+  elsif ($fileExt eq '.ico') { return 'image/x-icon'; }
+  elsif ($fileExt eq '.jpg') { return 'image/jpeg'; }
+  elsif ($fileExt eq '.png') { return 'image/png'; }
+  elsif ($fileExt eq '.gz') { return 'application/gzip'; }
+  else { return 'text/plain'; }
+}
+
 #process request
 sub processReq
 {
@@ -44,57 +113,95 @@ sub processReq
 
   my $timeOut = 3;  #socket timeout
   my $strLine;  #read line
-  my $strHTML = '';  #server response
-  my ($strMethod, $strURL, $strHost, $strReferer, $strAgent) = ('', '', '-', '-', '-');  #client request fields
-  my ($valSec, $valMin, $valHour, $valMday, $valMon, $valYear, $valWday, $valYday, $valIsdst) = gmtime();  #current UTC time
 
   while ($timeOut)
   {
-    while ($strLine = <$sockClient>)  #read line from client socket
+    if ($strLine = <$sockClient>)  #read line from client socket
     {
       $timeOut = 0;
       if ($strLine =~ /^GET\s\/\?year=(\d{4})&month=([a-z]+)&day=(\d{1,2})&eid=(\d+)\sHTTP/)  #smurt email request
       {
         my ($dateYear, $dateMonth, $dateDay, $idMail) = ($1, $2, $3, $4);
-        my $fileMail = sprintf("/var/log/mail/%d/%02d/%02d/mail-%d.txt", $dateYear, $valMonths{$dateMonth}, $dateDay, $idMail);
-        open(INMAIL, "<", $fileMail);
-        read INMAIL, $strHTML, -s INMAIL;  #read full file
-        close(INMAIL);
-	$strHTML =~ s/&/&amp;/sg;  #encode reserved &
-	$strHTML =~ s/</&lt;/sg;  #encode reserved <
-	$strHTML =~ s/>/&gt;/sg;  #encode reserved >
-	$strHTML = "<html><head><title>Email - $idMail</title></head><body><pre>$strHTML</pre></body></html>";
+        my $fileMail = sprintf('/var/log/mail/%d/%02d/%02d/mail-%d.txt', $dateYear, $valMonths{$dateMonth}, $dateDay, $idMail);
+        if (-e -s $fileMail)
+        {
+          my $strHTML;
+          my $inMail;
+          open($inMail, '<', $fileMail);
+          read($inMail, $strHTML, -s $inMail);  #read full file
+          close($inMail);
+          $strHTML =~ s/&/&amp;/sg;  #encode reserved &
+          $strHTML =~ s/</&lt;/sg;  #encode reserved <
+          $strHTML =~ s/>/&gt;/sg;  #encode reserved >
+          $strHTML = "<html><head><title>Email - $idMail</title></head><body><pre>$strHTML</pre></body></html>";
+
+          print $sockClient "HTTP/1.1 200 OK\r\n";
+          print $sockClient "Date: " . timeHTTP(time) . "\r\n";
+          print $sockClient "Server: Laconic\r\n";
+          print $sockClient "Last-Modified: " . timeHTTP((stat($fileMail))[9]) . "\r\n";
+          print $sockClient "Connection: Close\r\n";
+          print $sockClient "Content-Type: text/html\r\n";
+          print $sockClient "Content-Length: " . length($strHTML) . "\r\n\r\n";
+          print $sockClient $strHTML;
+        }
       }
       elsif ($strLine =~ /^(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)\s([^\s]+)\sHTTP/)
       {
-        $strMethod = $1;
-        $strURL = $2;
+        my ($strMethod, $strURL, $strHost, $strReferer, $strAgent, $strStatus, $fileHTML) = ($1, $2, '-', '-', '-', '404 Not Found', '/404.html');  #client request fields, default values
         $strURL =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
-        if ($strURL eq "/$strKnock")  #firewall open request
+        while ($strLine = <$sockClient>)
         {
-          my $iptClient = 'INPUT -i enp1s0 -p tcp -s ' . $sockClient->peerhost() . ' --dport 8000 -m state --state NEW -j ACCEPT';  #iptables allow client to splunk web
-          system("sudo iptables -C $iptClient || sudo iptables -A $iptClient");  #check if rule exists first
-          $strHTML = '<html><head><title>&pi;</title></head><body><h3>&pi;</h3></body></html>';
+          if ($strLine =~ /^Host:\s(.+)\r\n$/)
+          {
+            $strHost = $1;
+            $strHost =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+          }
+          elsif ($strLine =~ /^Referer:\s(.+)\r\n$/)
+          {
+            $strReferer = $1;
+            $strReferer =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+          }
+          elsif ($strLine =~ /^User-Agent:\s(.+)\r\n$/)
+          {
+            $strAgent = $1;
+            $strAgent =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+          }
         }
-	else
-	{
-          $strHTML = '<html><head><title>Under Construction</title></head><body><p>Website Currently Unavailable - Under Construction!</p></body></html>';
+
+        if ($strMethod eq 'GET')
+        {
+          if ($strURL eq '/')
+          {
+            $strStatus = '200 OK';
+            $fileHTML = '/index.html';
+          }
+          elsif (exists $filesRaw{$strURL})
+          {
+            $strStatus = '200 OK';
+            $fileHTML = $strURL;
+          }
+          elsif ($strURL eq "/$strKnock")  #firewall open request
+          {
+            my $iptClient = 'INPUT -i enp1s0 -p tcp -s ' . $sockClient->peerhost() . ' --dport 8000 -m state --state NEW -j ACCEPT';  #iptables allow client to splunk web
+            system("sudo iptables -C $iptClient || sudo iptables -A $iptClient");  #check if rule exists first
+            $strStatus = '201 Created';
+            $fileHTML = '/pi.html';
+          }
         }
-      }
-      elsif ($strLine =~ /^Host:\s(.+)\r\n$/)
-      {
-        $strHost = $1;
-        $strHost =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
-      }
-      elsif ($strLine =~ /^Referer:\s(.+)\r\n$/)
-      {
-        $strReferer = $1;
-        $strReferer =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
-      }
-      elsif ($strLine =~ /^User-Agent:\s(.+)\r\n$/)
-      {
-        $strAgent = $1;
-        $strAgent =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+
+        print $sockClient "HTTP/1.1 $strStatus\r\n";
+        print $sockClient "Date: " . timeHTTP(time) . "\r\n";
+        print $sockClient "Server: Laconic\r\n";
+        print $sockClient "Last-Modified: " . timeHTTP($filesMod{$fileHTML}) . "\r\n";
+        print $sockClient "Connection: Close\r\n";
+        print $sockClient "Content-Type: " . mimeHTTP($fileHTML) . "\r\n";
+        print $sockClient "Content-Length: " . length($filesRaw{$fileHTML}) . "\r\n\r\n";
+        print $sockClient $filesRaw{$fileHTML};
+
+        my $outLog;
+        open($outLog, '>>', '/var/log/http.log');  #open http log
+        print $outLog timeLog() . ' ' . $sockClient->peerhost() . ':' . $sockClient->peerport() . '->' . $sockClient->sockhost() . ":80 \"$strMethod $strURL\" \"$strHost\" \"$strReferer\" \"$strAgent\" " . substr($strStatus, 0, 3) . "\n";  #log http request
+        close($outLog);  #close log file
       }
     }
     if ($timeOut)
@@ -104,34 +211,9 @@ sub processReq
     }
   }
 
-  if ($strHTML ne '')  #server response
-  {
-    my $strSdate = sprintf("%s, %02d %s 20%02d %02d:%02d:%02d GMT", $txtDays[$valWday], $valMday, $txtMonths[$valMon], $valYear - 100, $valHour, $valMin, $valSec);  #server date
-    print $sockClient "HTTP/1.1 200 OK\r\n";
-    print $sockClient "Date: $strSdate\r\n";
-    print $sockClient "Server: Laconic\r\n";
-    print $sockClient "Last-Modified: $strSdate\r\n";
-    print $sockClient "Connection: Close\r\n";
-    print $sockClient "Content-Type: text/html\r\n";
-    print $sockClient "Content-Length: " . length($strHTML) . "\r\n\r\n";
-    print $sockClient $strHTML;
-  }
-
-  if ($strMethod ne '')
-  {
-    my $addrClient = $sockClient->peerhost() . ':' . $sockClient->peerport();  #client ip address:port
-    my $addrServer = $sockClient->sockhost() . ':80';  #server ip address:port
-    my $strLdate = sprintf("%s %02d %02d:%02d:%02d", $txtMonths[$valMon], $valMday, $valHour, $valMin, $valSec);  #log date
-    open(OUTLOG, ">>", '/var/log/http.log');  #open http log
-    print OUTLOG "$strLdate $addrClient->$addrServer \"$strMethod $strURL\" \"$strHost\" \"$strReferer\" \"$strAgent\"\n";  #write http request to log
-    close(OUTLOG);  #close log file
-  }
-
   shutdown($sockClient, SHUT_RDWR);  #shutdown client socket
   close($sockClient);  #close client socket
 }
-
-umask(0);  #allow permissions
 
 while (my $sockAccept = $sockListen->accept)
 {
--- download/smurt.pl
+++ download/smurt.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#  smurt.pl - Version 1.0.0  07 Nov 20
+#  smurt.pl - Version 1.0.1  14 Nov 20
 #  Smurt - SMTP Responder
 #  Copyright 2020 Del Castle
 #
@@ -10,12 +10,13 @@
 #    touch /var/log/smtp.log
 #    mkdir /var/log/mail
 #    chown www:www /usr/local/bin/smurt.pl /var/log/smtp.log /var/log/mail
-#    chmod 700 /usr/local/bin/smurt.pl
+#    chmod 750 /usr/local/bin/smurt.pl
 #    chmod 644 /var/log/smtp.log
-#    chmod 755 /var/log/mail
+#    chmod 750 /var/log/mail
 #    Add to /etc/crontab: * *  * * *  www  ps -ef | grep smurt.pl | grep -v grep >/dev/null || smurt.pl >/dev/null 2>&1 &
 #    Port forward 25 to 5123 on firewall
 
+use autodie;
 use strict;
 use warnings;
 use threads;
@@ -23,11 +24,9 @@ use Fcntl;
 use File::Path qw(make_path);
 use IO::Socket::INET;
 
-my $strServer = 'lab.server.com';  #server name
+my $strServer = 'smtp.server.com';  #server name
 my @txtDays = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
 my @txtMonths = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-
-my $cntMail = time();  #number to identify email
 
 #listening socket to receive email
 my $sockListen = IO::Socket::INET->new(LocalPort => 5123,
@@ -38,6 +37,8 @@ my $sockListen = IO::Socket::INET->new(LocalPort => 5123,
 #capture kill to exit safely
 $SIG{INT} = sub { close($sockListen); };
 $SIG{TERM} = sub { close($sockListen); };
+
+my $cntMail = time();  #number to identify email
 
 #process email
 sub processMail
@@ -54,11 +55,12 @@ sub processMail
   my ($strMTA, $strFrom, $strSubject) = ('', '-', '-');  #client email fields
   my ($valSec, $valMin, $valHour, $valMday, $valMon, $valYear, $valWday, $valYday, $valIsdst) = gmtime();  #current UTC time
 
-  my $filePath = sprintf("/var/log/mail/20%02d/%02d/%02d", $valYear - 100, $valMon + 1, $valMday);  #save file path
-  make_path($filePath, { mode => 0755 });  #create save file path if it doesn't exist
+  my $filePath = sprintf('/var/log/mail/20%02d/%02d/%02d', $valYear - 100, $valMon + 1, $valMday);  #save file path
+  make_path($filePath, { mode => 0750 });  #create save file path if it doesn't exist
   my $fileMail = "$filePath/mail-$idMail.txt";
-  open(OUTMAIL, ">", $fileMail);  #write email received to file
-  print OUTMAIL "$addrClient->$addrServer\r\n";
+  my $outMail;
+  open($outMail, '>', $fileMail);  #write email received to file
+  print $outMail "$addrClient->$addrServer\r\n";
 
   print $sockClient "220 $strServer SMTP Ready\r\n";
 
@@ -66,8 +68,8 @@ sub processMail
   {
     while ($strLine = <$sockClient>)  #read line from client socket
     {
-      print OUTMAIL $strLine;  #write line to file
-      print OUTMAIL "\r\n" if ($strLine !~ /\n$/);
+      print $outMail $strLine;  #write line to file
+      print $outMail "\r\n" if ($strLine !~ /\n$/);
  
       #reply to SMTP commands
       if ($strLine =~ /^HELO\s([\w\-\.]+)/i)
@@ -91,7 +93,7 @@ sub processMail
       elsif ($strLine =~ /^MAIL\sFROM:<(.+?)>/i)
       {
         $strFrom = $1;
-	$strFrom =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+        $strFrom =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
         print $sockClient "250 OK\r\n";
       }
       elsif ($strLine =~ /^(RCPT\sTO:|\.\r\n|RSET\r\n|SEND\sFROM:|SOML\sFROM:|SAML\sFROM:|VRFY\s|EXPN\s|NOOP|TURN\r\n)/i)
@@ -109,13 +111,13 @@ sub processMail
       elsif ($strLine =~ /^QUIT\r\n/i)
       {
         print $sockClient "221 Bye\r\n";
-	$timeOut = 0;
+        $timeOut = 0;
         last;
       }
       elsif ($strLine =~ /^Subject:\s?(.+)\r\n$/i)
       {
         $strSubject = $1 if ($strSubject eq '-');
-	$strSubject =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
+        $strSubject =~ s/(")/sprintf("%%%02X", ord($1))/seg;  #encode quotes
       }
     }
     if ($timeOut)
@@ -125,14 +127,14 @@ sub processMail
     }
   }
 
-  close(OUTMAIL);  #close email received file
+  close($outMail);  #close email received file
 
   if ($strMTA ne '')
   {
-    my $strLdate = sprintf("%s %02d %02d:%02d:%02d", $txtMonths[$valMon], $valMday, $valHour, $valMin, $valSec);  #log date
-    open(OUTLOG, ">>", '/var/log/smtp.log');  #open mail log
-    print OUTLOG "$strLdate $addrClient->$addrServer $idMail " . (-s $fileMail) . " \"$strMTA\" \"$strFrom\" \"$strSubject\"\n";  #write mail fields to log
-    close(OUTLOG);  #close log file
+    my $outLog;
+    open($outLog, '>>', '/var/log/smtp.log');  #open mail log
+    print $outLog sprintf('%s %02d %02d:%02d:%02d', $txtMonths[$valMon], $valMday, $valHour, $valMin, $valSec) . " $addrClient->$addrServer $idMail \"$strMTA\" \"$strFrom\" \"$strSubject\" " . (-s $fileMail) . "\n";  #log mail fields
+    close($outLog);  #close log file
   }
 
   shutdown($sockClient, SHUT_RDWR);  #shutdown client socket
